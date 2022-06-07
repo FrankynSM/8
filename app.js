09ea20d90066229da97d3c180900a5e0239f541d
FrankynSM <101581082+FrankynSM@users.noreply.github.com>
2022-06-06 20:06:39 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+// Objetos
+
+// Object Literal
+
+const persona = {
+ nombre: 'Frank',
+ profesion: 'Estudiante',
+ edad: 16,
+}
+const persona2 = {
+ nombre: 'Juan',
+ profesion: 'Desarrollar web',
+ edad: 500,
+}
+
+console.log(persona)
+console.log(persona.nombre)
+console.log(persona.profesion)
+console.log(persona.edad)
+console.log(persona2)
+console.log(persona2.nombre)
+console.log(persona2.profesion)
+console.log(persona2.edad)
